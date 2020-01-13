b44398ea877e02f57ef91d3ba0d83f687440e166
project-space-provisioner <azure-pipeline-project-space-provisioner@azure-devops.com>
2020-01-13 22:43:15 +0100

    Added automation for policy upload

---
+++ uploadCustomPolicies.ps1
@@ -0,0 +1,65 @@
+param(
+    [String]$directory,
+    [String]$tenant
+)
+
+function CheckForModule {
+
+    if(!(get-module AzureADPreview -ListAvailable)) {
+        try {
+            Write-Host "Installing AzureADPreview Module" -ForegroundColor Yellow
+            Install-Module AzureADPreview -force -AllowClobber
+            Import-module AzureADPreview
+        }catch{
+            Write-host "Could not install AzureAD Module because of : $($error.exception.message)" -ForegroundColor red
+            exit
+        }
+    }
+    Import-module AzureADPreview
+}
+
+if([String]::IsNullOrEmpty($directory)) {
+    $directory = $PSScriptRoot
+}
+
+function CheckForAzureConnection {
+    try{
+        get-azureadtenantDetail -ErrorAction stop | out-null
+    }catch{
+        try{
+            Write-host "First you have to connect to Azure AD" -ForegroundColor Yellow
+            if($tenant) {
+                Connect-AzureAD -TenantId $tenant -ErrorAction Stop
+            }else {
+                Connect-AzureAD -ErrorAction Stop
+            }
+        }catch{
+            Write-host "Could not connect to azureAd because : $($error.exception.message)" -ForegroundColor red
+            exit 1
+        }
+    }
+}
+
+function UploadPolicies {
+    Write-Progress -Activity "Uploading Custom Policies" -Status "Getting Files" -PercentComplete 0 
+    $files = gci $directory | ? {$_.Name -like "*B2C*"}
+    $counter = 0
+    foreach($file in $files) {
+        try{
+            $counter++
+            $id = $file.Name.Substring($file.Name.IndexOf("B2C")).Split('.')[0]
+            Write-Progress -Activity "Uploading Custom Policies" -Status "Uploading $id" -PercentComplete (($counter/$files.count) * 100)
+            Set-AzureADMSTrustFrameworkPolicy -Id $id -InputFilePath $file.FullName | Out-Null
+            Write-host "Successfully updated $id" -ForegroundColor Green
+        }catch{
+            Write-host "Could not set Policy : $($file.name) because of : $($error.exception.message)" -ForegroundColor Red
+        }
+    }
+}
+function main {
+    CheckForModule
+    CheckForAzureConnection
+    UploadPolicies
+}
+
+main
